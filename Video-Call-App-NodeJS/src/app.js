a2389c3f8916efbb22f25b5ce1fe6782b7045206
임유빈 <p.yoobinbkk@naver.com>
2022-12-20 18:21:29 +0900

    3rd Commit - 2022.12.20 화요일

--- Video-Call-App-NodeJS/src/app.js
+++ Video-Call-App-NodeJS/src/app.js
@@ -1,10 +1,26 @@
 let express = require( 'express' );
 let app = express();
-let server = require( 'http' ).Server( app );
-let io = require( 'socket.io' )( server );
+const https = require('https');
+const fs = require("fs");
+// let server = https.Server( app );
 let stream = require( './ws/stream' );
 let path = require( 'path' );
 let favicon = require( 'serve-favicon' );
+// const { fstat } = require('fs');
+
+let server = https
+    .createServer(
+        {
+            key: fs.readFileSync("key.pem"),
+            cert: fs.readFileSync("cert.pem"),
+        },
+        app
+    )
+    .listen(3000, ()=> {
+        console.log('server is running at port 3000')
+    });
+
+let io = require( 'socket.io' )( server );
 
 app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
@@ -16,4 +32,4 @@ app.get( '/', ( req, res ) => {
 
 io.of( '/stream' ).on( 'connection', stream );
 
-server.listen( 3000 );
+// server.listen( 3000 );
